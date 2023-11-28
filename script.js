57bcf41ca0884dd9d3136be1941bdad003f2f371
Abraham TireniOluwa Olamide <121473246+Tech-Reni@users.noreply.github.com>
2023-11-28 23:48:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let hamburger = document.querySelector('.hamburger');
+let mbl_nav_cont = document.querySelector('.mobile-nav-container');
+let close_btn = document.querySelector('.close');
+
+hamburger.addEventListener('click', () => {
+    mbl_nav_cont.classList.toggle('active');
+    hamburger.style.display = 'none';
+});
+
+close_btn.addEventListener('click', () => {
+    mbl_nav_cont.classList.toggle('active');
+    hamburger.style.display = 'block';
+})
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", (e) => {
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle("show-text");
+    })
+})
+
+
+ScrollReveal({
+    distance: '150px',
+    duration: 1700,
+    delay: 10
+});
+
+ScrollReveal().reveal('.article', { origin: 'right' });
+ScrollReveal().reveal('.card', { origin: 'left' });
+ScrollReveal().reveal('.question', { origin: 'right' });
+ScrollReveal().reveal('.left', { origin: 'right' });
+ScrollReveal().reveal('.right', { origin: 'left' });
+ScrollReveal().reveal('.price-card', { origin: 'left' });
